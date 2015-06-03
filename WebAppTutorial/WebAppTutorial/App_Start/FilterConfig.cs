8ebd204c853f33b513c70b225d72f008b3d2f02e
Jarvitta Massey <jamssey@batson-cook.com>
2015-06-03 12:51:33 -0400

    Hello Commit Message

---
+++ WebAppTutorial/WebAppTutorial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTutorial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebAppTutorial/WebAppTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTutorial.Startup))]
+namespace WebAppTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
